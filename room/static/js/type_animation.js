4ca1adcd3564e6912829759eab34fcaaaa25f0db
Carl <carl.almayda@stratodigital.io>
2020-08-30 22:01:29 +0800

    Paraphrase a phrase

--- room/static/js/type_animation.js
+++ room/static/js/type_animation.js
@@ -3,7 +3,7 @@ import Typed from '/static/js/typed.js';
 $('document').ready(function(){
 
     var options = {
-        strings: ["hi, i'm carl. nice to meet you.", "i work as a software developer.",  "i like sweets.", "i like it simple and minimal.", "i love watching movies.", "i love challenges."],
+        strings: ["hi, i'm carl. nice to meet you.", "i work as a software engineer.",  "i like sweets.", "i like it simple and minimal.", "i love watching movies.", "i love challenges."],
         typeSpeed: 60,
         loop: !0,
         backDelay: 1500,
