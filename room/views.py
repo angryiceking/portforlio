6ff1f8bf26e04fd4ed63199c1dba9561b95583e4
Carl <carl.almayda@stratodigital.io>
2020-09-03 12:34:30 +0800

    Add csrf_exempt token

--- room/views.py
+++ room/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import ListView, DetailView, View
 
 # Create your views here.
@@ -8,6 +9,7 @@ class Lounge(View):
     def get(self, request):
         return render(request, 'index.html', status=200)
 
+    @csrf_exempt
     def post(self, request):
         return HttpResponse('congrats!', status=200)
         
