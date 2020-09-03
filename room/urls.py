15191f5325101a672135585aa158c90b848df362
Carl <carl.almayda@stratodigital.io>
2020-09-03 12:40:17 +0800

    Update csrf_exempt in urls

--- room/urls.py
+++ room/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from .views import *
+from django.views.decorators.csrf import csrf_exempt
 
 app_name = 'room'
 
 urlpatterns = [
-    path('', Lounge.as_view(), name='lounge'),
+    path('', csrf_exempt(Lounge.as_view()), name='lounge'),
 ]
